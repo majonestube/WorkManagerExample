f283ec91ac516ef67e8a77a4e127847ac1073e42
MajonesTube <martine.vold@hotmail.com>
2025-04-12 20:39:26 +0200

    Fullført app

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -83,6 +83,12 @@ dependencies {
     implementation("androidx.work:work-runtime-ktx:2.10.0")
     // WorkManager dependency
     implementation("androidx.work:work-runtime-ktx:2.8.1")
+    // Espresso
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    // Junit
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    // Work testing
+    androidTestImplementation("androidx.work:work-testing:2.8.1")
 
     debugImplementation("androidx.compose.ui:ui-test-manifest")
     debugImplementation("androidx.compose.ui:ui-tooling")
